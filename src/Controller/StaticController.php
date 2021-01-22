6dfc34c09667cac3f0916b963b5727c368b3547a
AzKam <azizkamadou17@gmail.com>
2021-01-22 17:10:59 +0000

    Contact page finished, seems that there is a firewall blocking mails sended from code.

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Repository\PageCategoryRepository;
+use Psr\Log\LoggerInterface;
+use Swift_Mailer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,7 +14,7 @@ class StaticController extends AbstractController
 {
 
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="homepage", methods={"GET"})
      * @param PageCategoryRepository $pageCatRepo
      * @return Response
      */
@@ -26,7 +29,7 @@ class StaticController extends AbstractController
     }
 
     /**
-     * @Route("/presentation", name="presentation")
+     * @Route("/presentation", name="presentation", methods={"GET"})
      * @return Response
      */
     public function presentation(): Response
@@ -35,11 +38,59 @@ class StaticController extends AbstractController
     }
 
     /**
-     * @Route("/contact", name="contact")
+     * @Route("/contact", name="contact", methods={"GET"})
      * @return Response
      */
     public function contact(): Response
     {
         return $this->render('static/contact.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="mail_contact", methods={"POST"})
+     * @param Request $request
+     * @param Swift_Mailer $mailer
+     * @param LoggerInterface $logger
+     * @return Response
+     */
+    public function contact_mail(Request $request, Swift_Mailer $mailer, LoggerInterface $logger) : Response
+    {
+        $success = ['primary', 'Votre message a été envoyé, nous vous répondons au plus vite.'];
+        $error = ['danger', 'Echec, veuillez réessayer plus tard.'];
+
+        $status = $error;
+        //$logger->info('++++++++++++++++++    '.$request->query->get("name").' - '. $request->query->get("email").' - '.$request->query->get("message"));
+        $senderName = $request->request->get("name") ?? false;
+        $senderEmail = $request->request->get("email") ?? false;
+        $message = $request->request->get("message") ?? false;
+        $logger->info('++++++++++++++++++    '.$senderName.' - '. $senderEmail.' - '.$message);
+        if ($message || $senderEmail || $senderName)
+        {
+
+            $message = (new \Swift_Message('[AUDIMAF] Nouveau message de '.$senderName.' - '.$senderEmail))
+                ->setFrom("contact@audimaf.com")
+                ->setTo("contact@audimaf.com")
+                ->setBody(
+                    $this->renderView(
+                        'emails/contact.html.twig',
+                        [
+                            'senderName' => $senderName,
+                            'senderEmail' => $senderEmail,
+                            'message' => $message
+                        ]
+                    ),
+                    'text/html'
+                )
+            ;
+            $mailer->send($message);
+            unset($message, $senderName, $senderEmail, $message, $mailer);
+            $status = $success;
+        }
+
+        $this->addFlash(
+            $status[0],
+            $status[1]
+        );
+        return $this->redirectToRoute("contact");
     }
 }
